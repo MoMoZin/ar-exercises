735d0407eeeabee87976ce1414834d6f7b45b983
kmomozin <khinmomozin@gmail.com>
2023-03-06 15:57:32 -0700

    link store and employee table and add employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Viktor", last_name: "Krum", hourly_rate: 60)
+@store1.employees.create(first_name: "Harry", last_name: "Potter", hourly_rate: 60)
+@store1.employees.create(first_name: "Ron", last_name: "Weasley", hourly_rate: 60)
+@store2.employees.create(first_name: "Hermione", last_name: "Granger", hourly_rate: 60)
+@store2.employees.create(first_name: "Ginny", last_name: "Weasley", hourly_rate: 60)
+@store2.employees.create(first_name: "Lily", last_name: "Potter", hourly_rate: 60)
